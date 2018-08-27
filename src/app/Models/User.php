82092c1479c0cf8b20fbb0e0246eef2f658553bf
Ivan Serpa <ivan.serpa@hotmail.com>
2018-08-26 22:51:47 -0300

    User model changes
    Change the file location.
    Add trait to help the pagination.
    Automate the password hash.

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Traits\Paginable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Paginable;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +28,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
 }
