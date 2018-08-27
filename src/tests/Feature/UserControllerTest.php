4153af9072aa8b7d9f3abc80765e66e459f5079a
Ivan Serpa <ivan.serpa@hotmail.com>
2018-08-27 14:42:38 -0300

    Basic get test

--- src/tests/Feature/UserControllerTest.php
+++ src/tests/Feature/UserControllerTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /**
      * A basic test example.
      *
@@ -16,8 +16,27 @@ class UserControllerTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        // $response = $this->get('/');
+        $response = $this->get(route('users.index'));
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+                 ->assertJsonStructure([
+                    'current_page',
+                    'data',
+                    'first_page_url',
+                    'from',
+                    'last_page',
+                    'last_page_url',
+                    'next_page_url',
+                    'path',
+                    'per_page',
+                    'prev_page_url',
+                    'to',
+                    'total',
+                 ])
+                 ->assertJson([
+                    'current_page' => 1,
+                    'per_page' => 150,
+                 ]);
     }
 }
